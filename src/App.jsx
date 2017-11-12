1ec6922efcd8cc9b20d1f663ade1e246f6fc9a91
Sergey <sergeykozlov.2011@yandex.ru>
2017-11-12 16:33:50 +0300

    add structure app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,36 @@ import './app.sass';
 class App extends React.Component {
     render(){
         return(
-          <p>App</p>
+            <div className="notes__wrapper">
+                <div className="container">
+                    <form className='note__form'>
+                        <textarea className='note__textarea' placeholder='Введи заметку'></textarea>
+                        <button className="btn__add material-icons">note_add</button>
+                    </form>
+                    <div className="notes__container">
+                        <div className="note_item">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, soluta.</p>
+                            <button className="btn__close material-icons close">close</button>
+                        </div>
+                        <div className="note_item">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, soluta.</p>
+                            <button className="btn__close material-icons close">close</button>
+                        </div>
+                        <div className="note_item">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, soluta.</p>
+                            <button className="btn__close material-icons close">close</button>
+                        </div>
+                        <div className="note_item">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, soluta.</p>
+                            <button className="btn__close material-icons close">close</button>
+                        </div>
+                        <div className="note_item">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, soluta.</p>
+                            <button className="btn__close material-icons close">close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
